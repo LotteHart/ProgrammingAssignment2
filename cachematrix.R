59a14ac4b652db0fb12bab54201ff85c599ef569
Lotte Hart <lotte.hart@seederdeboer.nl>
2023-05-12 10:30:14 +0200

    Created function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# R program to find inverse of a Matrix
+
+# Create a matrix
+# Create 3 different vectors
+# using combine method.
+a1 <- c(3, 2, 5)
+a2 <- c(2, 3, 2)
+a3 <- c(5, 2, 4)
+
+# bind the three vectors into a matrix 
+A <- rbind(a1, a2, a3)
+
+# print the original matrix
+print(A)
+
+# Use the solve() function 
+# to calculate the inverse.
+T1 <- solve(A)
+
+# print the inverse of the matrix.
+print(T1)
+
+
+## makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## CacheSolve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
